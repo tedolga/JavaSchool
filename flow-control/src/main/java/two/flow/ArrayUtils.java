9844703be5d4a5d2d5bc0380d25a42b6d1b45e6e
Tedikova <tedolga@gmail.com>
2012-04-02 01:40:03 +0400

    Array sorting

---
+++ flow-control/src/main/java/two/flow/ArrayUtils.java
@@ -0,0 +1,12 @@
+package two.flow;
+
+/**
+ * @author Tedikova O.
+ * @version 1.0
+ */
+public class ArrayUtils {
+
+    public static int[] quickSort(int[] array) {
+        return new int[array.length];
+    }
+}
