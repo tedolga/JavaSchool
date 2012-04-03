de89a5b2f3f5c1dc22002212e3b2a3c0d9ae1222
Tedikova Olga <tedolga@gmail.com>
2012-04-03 18:53:52 +0400

    Merge sort was created

--- flow-control/src/main/java/two/flow/ArrayUtils.java
+++ flow-control/src/main/java/two/flow/ArrayUtils.java
@@ -10,6 +10,35 @@ public class ArrayUtils {
         return quickSort1(array, 0, array.length - 1);
     }
 
+    public static int[] mergeSort(int[] array) {
+        return mergeSort1(array, 0, array.length);
+    }
+
+    private static int[] mergeSort1(int[] array, int off, int len) {
+        if (array.length > 1) {
+            int splitIndex = array.length / 2;
+            int[] firstArray = new int[splitIndex];
+            System.arraycopy(array, 0, firstArray, 0, splitIndex);
+            int[] secondArray = new int[array.length - splitIndex];
+            System.arraycopy(array, splitIndex, secondArray, 0, array.length - splitIndex);
+            firstArray = mergeSort1(firstArray, 0, firstArray.length);
+            secondArray = mergeSort1(secondArray, 0, secondArray.length);
+            int[] resultArray = new int[firstArray.length + secondArray.length];
+            for (int i = 0, j = 0, k = 0; k < resultArray.length; k++) {
+                if (j >= secondArray.length || (i < firstArray.length && firstArray[i] <= secondArray[j])) {
+                    resultArray[k] = firstArray[i];
+                    i++;
+                } else {
+                    resultArray[k] = secondArray[j];
+                    j++;
+                }
+            }
+            return resultArray;
+        } else {
+            return array;
+        }
+    }
+
     private static int[] quickSort1(int[] array, int start, int end) {
         int i = start;
         int j = end;
@@ -21,7 +50,7 @@ public class ArrayUtils {
             while (array[j] > refElement) {
                 j--;
             }
-            swap(array[i], array[j]);
+            swap(array, i, j);
             i++;
             j--;
         }
@@ -34,9 +63,9 @@ public class ArrayUtils {
         return array;
     }
 
-    private static void swap(int first, int second) {
-        int temp = second;
-        second = first;
-        first = temp;
+    private static void swap(int[] array, int first, int second) {
+        int temp = array[second];
+        array[second] = array[first];
+        array[first] = temp;
     }
 }
--- flow-control/src/test/java/two/flow/ArrayUtilsTest.java
+++ flow-control/src/test/java/two/flow/ArrayUtilsTest.java
@@ -11,6 +11,15 @@ public class ArrayUtilsTest {
     @Test
     public void testQuickSort() throws Exception {
         Assert.assertArrayEquals(new int[]{2, 3, 5, 6, 9}, ArrayUtils.quickSort(new int[]{3, 5, 6, 9, 2}));
-
+        Assert.assertArrayEquals(new int[]{2, 2}, ArrayUtils.quickSort(new int[]{2, 2}));
+        Assert.assertArrayEquals(new int[]{2, 2, 2, 2, 5}, ArrayUtils.quickSort(new int[]{2, 2, 5, 2, 2}));
     }
+
+    @Test
+    public void testMergeSort() throws Exception {
+        Assert.assertArrayEquals(new int[]{2, 3, 5, 6, 9}, ArrayUtils.mergeSort(new int[]{3, 5, 6, 9, 2}));
+        Assert.assertArrayEquals(new int[]{2, 2}, ArrayUtils.mergeSort(new int[]{2, 2}));
+        Assert.assertArrayEquals(new int[]{2, 2, 2, 2, 5}, ArrayUtils.mergeSort(new int[]{2, 2, 5, 2, 2}));
+    }
+
 }
