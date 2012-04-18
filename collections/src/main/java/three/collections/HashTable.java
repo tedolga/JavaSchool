00381c8f37efbb38e5ecff099a20be78718ce0cd
Tedikova Olga <tedolga@gmail.com>
2012-04-18 12:34:40 +0400

    Hashtable was renewed

--- collections/src/main/java/three/collections/HashTable.java
+++ collections/src/main/java/three/collections/HashTable.java
@@ -10,37 +10,62 @@ public class HashTable {
     private static final float OVERLOAD_LIMIT = 0.75f;
     private LinkedList[] buckets;
     private int capacity;
+    private int mod;
 
     public HashTable(int initialCapacity) {
         this.capacity = initialCapacity;
         buckets = new LinkedList[capacity];
+        mod = capacity;
     }
 
     public Object put(Object key, Object value) {
+        checkNotNull("Key", key);
+        checkNotNull("Value", value);
         int elementIndex = calculateIndex(key.hashCode());
-        LinkedList values = buckets[elementIndex];
+        Object returnValue = putToBucket(elementIndex, key, value);
+        if (buckets[elementIndex].size() > OVERLOAD_LIMIT * capacity) {
+            rehash();
+        }
+        return returnValue;
+    }
+
+
+    public int calculateIndex(int hashCode) {
+        int index = Math.abs(hashCode);
+        while (index > mod) {
+            index = index % mod;
+        }
+        return index;
+    }
+
+    private void checkNotNull(String argName, Object argValue) {
+        if (argValue == null) {
+            throw new NullPointerException(String.format("%s must not be null.", argName));
+        }
+    }
+
+    private Object putToBucket(int index, Object key, Object value) {
+        LinkedList values = buckets[index];
         if (values != null) {
             for (Object next : values) {
                 TableElement element = (TableElement) next;
-                if (key.equals(element.key)) {
+                if (element.key.equals(key)) {
                     Object oldValue = element.value;
                     element.value = value;
                     return oldValue;
                 }
-                values.add(new TableElement(key, value));
             }
+            values.add(new TableElement(key, value));
+            return null;
         } else {
-
+            buckets[index] = new LinkedList<TableElement>();
+            buckets[index].add(new TableElement(key, value));
+            return null;
         }
-        return null;
     }
 
-    public int calculateIndex(int hashCode) {
-        int index = Math.abs(hashCode);
-        while (index > capacity) {
-            index = index % capacity;
-        }
-        return index;
+    private void rehash() {
+
     }
 
     private static class TableElement {
--- collections/src/test/java/three/collections/HashTableTest.java
+++ collections/src/test/java/three/collections/HashTableTest.java
@@ -9,6 +9,17 @@ import org.junit.Test;
  */
 public class HashTableTest {
     @Test
+    public void testPut() throws Exception {
+        HashTable table = new HashTable(10);
+        Assert.assertNull(table.put("1", 1));
+        Assert.assertNull(table.put("2", 2));
+        Assert.assertNull(table.put("3", 3));
+        Assert.assertEquals(3, table.put("3", "3"));
+        Assert.assertEquals(2, table.put("2", "2"));
+        Assert.assertNull(table.put(51, 3));
+    }
+
+    @Test
     public void testCalculateIndex() throws Exception {
         HashTable table = new HashTable(1);
         Assert.assertEquals(0, table.calculateIndex(28));
