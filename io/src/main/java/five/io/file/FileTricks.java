ce9dc77964691e8e4a0c5a44563901b5843e5c0e
Tedikova <tedolga@gmail.com>
2012-04-24 02:53:31 +0400

    files

---
+++ io/src/main/java/five/io/file/FileTricks.java
@@ -0,0 +1,38 @@
+package five.io.file;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+
+/**
+ * @author Tedikova O.
+ * @version 1.0
+ */
+public class FileTricks {
+    public static void main(String[] args) throws IOException {
+        File file = new File("file.txt");
+        if (!file.exists()) {
+            System.out.println("File was created: " + file.createNewFile());
+        }
+        System.out.println("File was deleted: " + file.delete());
+        System.out.println("File was recreated: " + file.createNewFile());
+
+        File dir = new File("src/main/java/five/io/file/");
+        System.out.println("Files in " + dir.getAbsolutePath() + ":");
+        String[] list = dir.list();
+        for (String next : list) {
+            System.out.println(next);
+        }
+        list = dir.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".java");
+            }
+        });
+
+        System.out.println("Source files in " + dir.getAbsolutePath() + ":");
+        for (String next : list) {
+            System.out.println(next);
+        }
+    }
+}
